6b194350ea340955f8736ce82c99a6893346f551
Glitch (oasis-cold) <none>
2019-06-04 23:22:49 -0400

    add directional thumbnails

--- components/set-image.js
+++ components/set-image.js
@@ -18,12 +18,20 @@ AFRAME.registerComponent('set-image', {
 
     this.setupFadeAnimation();
 
+    var entityList = document.getElementsByClassName("entity-group");
+
     el.addEventListener(data.on, function () {
       // Fade out image.
       data.target.emit('set-image-fade');
       // Wait for fade to complete.
       setTimeout(function () {
         // Set image.
+        // console.log(data.src);
+        for(var i = 0; i < entityList.length; i ++){
+          entityList[i].setAttribute("scale", "0 0 0");
+        }
+        var targetScene = document.getElementById(data.src);
+        targetScene.setAttribute("scale", "1 1 1");
         data.target.setAttribute('material', 'src', data.src);
       }, data.dur);
     });
